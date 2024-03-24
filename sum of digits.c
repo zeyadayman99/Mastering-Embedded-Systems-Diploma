20d58b1f8ccc0285960a3415e131b19104c513a3
Zeyad Ayman <ziadayman095@gmail.com>
2024-03-24 23:40:07 +0200

    adding new assignment

---
+++ sum of digits.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int sum_of_digits (int num) {
+	int sum = 0;
+	for (int i = num; i > 0; i++) {
+		sum += (num%10);
+		num /= 10;
+
+	}
+
+	return sum;
+}
+int main() {
+	int num;
+	printf("please enter the number to find sum: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d", &num);
+	printf("sum of %d digits = %d", num, sum_of_digits (num));
+	return 0;
+}
